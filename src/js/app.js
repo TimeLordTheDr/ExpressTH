d3fa9e7a0fa0c4d0e807921416cae0b1195487f3
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-11-13 19:39:46 +0200

    rendering HTML. Passing vars to templates

--- src/js/app.js
+++ src/js/app.js
@@ -4,22 +4,27 @@ const app = express();
 
 let name = 'Paul';
 
+const colors = ['red',
+'blue',
+'green'];
+
 app.set('view engine', 'pug');
 
 app.set('views', 'src/views');
 
+app.locals.position = 'NodeJS';
 
 app.get('/', (req, res)=>{
     res.render('index', { title: 'Altered title', message: 'Altered message' });//Somehow this obj params has no effect
 });
 
+app.get('/hello', (req, res)=>{
+    res.locals.score =10;
+    res.render('card', {name: 'Paul', colors})
+});
 
 app.get('/root', (req, res)=>{
     res.send("<h1>This is the root route! It appears we don't need the dist folder yet to reload the page. Which task runs first?</h1>");
-});
-
-app.get('/hello', (req, res)=>{
-    res.send(`<p>Hello ${name}. You are an awesome developer ${name}!!!</p>`);
 });
 
 let dt = Date();
